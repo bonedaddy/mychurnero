e4eee93093932c66da0ba4bfd22223e46fabfd9e
postables <postables@rtradetechnologies.com>
2020-09-02 00:49:42 -0700

    client: add sweep all function

--- client/sweep.go
+++ client/sweep.go
@@ -6,5 +6,23 @@ func (c *Client) SweepDust(walletName string) (*wallet.ResponseSweepDust, error)
 	if err := c.OpenWallet(walletName); err != nil {
 		return nil, err
 	}
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	return c.mw.SweepDust(&wallet.RequestSweepDust{GetTxHey: true, GetTxKeys: true})
+}
+
+func (c *Client) SweepAll(walletName, destAddress string, accountIndex uint64) (*wallet.ResponseSweepAll, error) {
+	if err := c.OpenWallet(walletName); err != nil {
+		return nil, err
+	}
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.mw.SweepAll(&wallet.RequestSweepAll{
+		Address:      destAddress,
+		AccountIndex: accountIndex,
+		Mixin:        10,
+		RingSize:     11,
+		GetTxHex:     true,
+		GetTxKeys:    true,
+	})
 }
