b392e151b406fb12118b3ec80bc274b274d7d1dd
postables <postables@rtradetechnologies.com>
2020-09-02 00:50:11 -0700

    add sweep, transfer, and refresh commands

--- cmd/mychurnero/main.go
+++ cmd/mychurnero/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/bonedaddy/mychurnero/client"
+	"github.com/monero-ecosystem/go-monero-rpc-client/wallet"
 	"github.com/urfave/cli/v2"
 )
 
@@ -15,6 +16,50 @@ func main() {
 	app.Usage = "automated churning application"
 	app.Description = "mychurnero is an automated churning application designed to reduce the overhead in churning and create a totally automatic solution. this application provides no guarantee in benefits, and should be used with caution, and with great care"
 	app.Commands = cli.Commands{
+		&cli.Command{
+			Name:  "transfer",
+			Usage: "transfer funds to address",
+			Action: func(c *cli.Context) error {
+				cl, err := client.NewClient(c.String("wallet.rpc_address"))
+				if err != nil {
+					return err
+				}
+				if err := cl.Transfer(c.String("wallet.name"), c.String("dest.address"), wallet.Float64ToXMR(c.Float64("value"))); err != nil {
+					return err
+				}
+				return cl.Close()
+			},
+		},
+		&cli.Command{
+			Name:  "refresh",
+			Usage: "refresh accounts",
+			Action: func(c *cli.Context) error {
+				cl, err := client.NewClient(c.String("wallet.rpc_address"))
+				if err != nil {
+					return err
+				}
+				if err := cl.Refresh(c.String("wallet.name")); err != nil {
+					return err
+				}
+				return cl.Close()
+			},
+		},
+		&cli.Command{
+			Name:  "sweep-all",
+			Usage: "sweep all accounts",
+			Action: func(c *cli.Context) error {
+				cl, err := client.NewClient(c.String("wallet.rpc_address"))
+				if err != nil {
+					return err
+				}
+				resp, err := cl.SweepAll(c.String("wallet.name"), c.String("dest.address"), c.Uint64("account.index"))
+				if err != nil {
+					return err
+				}
+				fmt.Printf("%#v\n", resp)
+				return cl.Close()
+			},
+		},
 		&cli.Command{
 			Name:  "sweep-dust",
 			Usage: "sweeps all dust",
@@ -46,7 +91,7 @@ func main() {
 						if err := cl.StartMining(c.String("wallet.name"), c.Uint64("threads")); err != nil {
 							return err
 						}
-						return nil
+						return cl.Close()
 					},
 					Flags: []cli.Flag{
 						&cli.Uint64Flag{
@@ -85,6 +130,23 @@ func main() {
 			Aliases: []string{"wrpc"},
 			Usage:   "the endpoint address of the monero-wallet-rpc server",
 			Value:   "http://127.0.0.1:6061/json_rpc",
+		},
+		&cli.StringFlag{
+			Name:    "dest.address",
+			Aliases: []string{"da"},
+			Usage:   "destination address to send funds to",
+			Value:   "BhJQR4hu54wAqx9iRZZv5Y1UcTV6qgH52ULy5UNpEn7B7HVT2jpmAttf1k7mARTVWASvZkvajTk2NT5c2x3JHmojB5BDrFV",
+		},
+		&cli.Uint64Flag{
+			Name:    "account.index",
+			Aliases: []string{"ai"},
+			Usage:   "account index to use",
+			Value:   0,
+		},
+		&cli.Float64Flag{
+			Name:  "value",
+			Usage: "value to use generally for transfers",
+			Value: 0.1,
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
