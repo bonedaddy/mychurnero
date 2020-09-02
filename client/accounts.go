20660a7934dc4964a739f0c4d7edc48349478989
postables <postables@rtradetechnologies.com>
2020-09-02 01:05:34 -0700

    client: add sub address fetch

--- client/accounts.go
+++ client/accounts.go
@@ -10,3 +10,15 @@ func (c *Client) GetAllAccounts(walletName string) (*wallet.ResponseGetAccounts,
 	defer c.mux.Unlock()
 	return c.mw.GetAccounts(&wallet.RequestGetAccounts{})
 }
+
+func (c *Client) GetAddress(walletName string, accountIndex uint64, addressIndex ...uint64) (*wallet.ResponseGetAddress, error) {
+	if err := c.OpenWallet(walletName); err != nil {
+		return nil, err
+	}
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.mw.GetAddress(&wallet.RequestGetAddress{
+		AccountIndex: accountIndex,
+		AddressIndex: addressIndex,
+	})
+}
