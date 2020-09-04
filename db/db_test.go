9c567cfc0789c6e2d3efc0d61ebb35cb18500941
postables <postables@rtradetechnologies.com>
2020-09-04 00:56:45 -0700

    db: start cleaning up tests and moving to table testing

--- db/db_test.go
+++ db/db_test.go
@@ -62,3 +62,58 @@ func TestDB(t *testing.T) {
 
 	require.True(t, updatedAt2.After(updatedAt))
 }
+
+func TestTransaction(t *testing.T) {
+	db, err := NewClient(dbPath)
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		os.RemoveAll(dbPath)
+	})
+
+	defer func() {
+		err := db.Destroy()
+		if err != nil {
+			t.Error(err)
+		}
+		err = db.Close()
+		require.NoError(t, err)
+	}()
+
+	err = db.Setup()
+	require.NoError(t, err)
+
+	type args struct {
+		sender   string
+		metadata string
+		sendTime time.Time
+		spent    uint
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantErr     bool
+		wantTxCount int
+	}{
+		{"1", args{"1", "1", time.Now().AddDate(0, 0, -1), 1}, false, 1},
+		{"2", args{"2", "2", time.Now().Add(time.Hour), 0}, false, 2},
+		{"3", args{"3", "3", time.Now().Add(time.Hour * 10), 0}, false, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			err := db.AddTransaction(tt.args.sender, tt.args.metadata, tt.args.sendTime)
+			require.NoError(t, err)
+
+			tx, err := db.GetTransaction(tt.args.sender)
+			require.NoError(t, err)
+			require.Equal(t, tx.TxMetadata, tt.args.metadata)
+			require.Equal(t, int(tx.Spent), 0)
+			require.True(t, tx.SendTime.Equal(tt.args.sendTime))
+
+			txs, err := db.GetTransactions()
+			require.NoError(t, err)
+			require.Len(t, txs, tt.wantTxCount)
+		})
+	}
+}
