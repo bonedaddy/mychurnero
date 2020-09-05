d44666d8e6137836261a9bab8ddd37f3d6011f5b
bonedaddy <boned4ddy@protonmail.com>
2020-09-05 13:34:01 -0700

    client: add additional sweep controls

--- client/sweep.go
+++ client/sweep.go
@@ -13,16 +13,47 @@ func (c *Client) SweepDust(walletName string) (*wallet.ResponseSweepDust, error)
 }
 
 // SweepAll is used to sweep all funds from the given account index sending it to the destination address
-func (c *Client) SweepAll(walletName, destAddress string, accountIndex uint64) (*wallet.ResponseSweepAll, error) {
-	if err := c.OpenWallet(walletName); err != nil {
+func (c *Client) SweepAll(opts TransferOpts) (*wallet.ResponseSweepAll, error) {
+	if err := c.OpenWallet(opts.WalletName); err != nil {
 		return nil, err
 	}
+	var addr string
+	for k := range opts.Destinations {
+		addr = k
+	}
 	return c.mw.SweepAll(&wallet.RequestSweepAll{
-		Address:      destAddress,
-		AccountIndex: accountIndex,
-		Mixin:        10,
-		RingSize:     11,
-		GetTxHex:     true,
-		GetTxKeys:    true,
+		Address:        addr,
+		AccountIndex:   opts.AccountIndex,
+		SubaddrIndices: opts.SubaddrIndices,
+		Priority:       opts.Priority,
+		Mixin:          10,
+		RingSize:       11,
+		GetTxHex:       true,
+		GetTxKeys:      true,
+		GetTxMetadata:  true,
+		DoNotRelay:     opts.DoNotRelay,
+	})
+}
+
+// SweepSingle is used to spend all of a specified unlocked output to an address
+func (c *Client) SweepSingle(opts TransferOpts) (*wallet.ResponseSweepSingle, error) {
+	if err := c.OpenWallet(opts.WalletName); err != nil {
+		return nil, err
+	}
+	var addr string
+	for k := range opts.Destinations {
+		addr = k
+	}
+	return c.mw.SweepSingle(&wallet.RequestSweepSingle{
+		Address:        addr,
+		AccountIndex:   opts.AccountIndex,
+		SubaddrIndices: opts.SubaddrIndices,
+		Priority:       opts.Priority,
+		Mixin:          10,
+		RingSize:       11,
+		GetTxHex:       true,
+		GetxKeys:       true,
+		GetTxMetadata:  true,
+		DoNotRelay:     opts.DoNotRelay,
 	})
 }
