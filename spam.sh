fa0f37546fc90e999ae8017781c562b520e6a6ce
bonedaddy <boned4ddy@protonmail.com>
2020-09-07 16:33:13 -0700

    chore: tree in spam script and config file

---
+++ spam.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+while true; do 
+	./mychurnero --dest.address BhnUnyq6BEYD5tfnAeqmviT33tL9Wbwoq6NaFzfacgtuAfLTcvden8R4g8ZGyeaRTGWAJm97zB92YFZdivY3D86sVcGGZp4 --account.index 0 --subaddr.indices 0 --value 0.5 transfer 
+	./mychurnero --dest.address BhJQR4hu54wAqx9iRZZv5Y1UcTV6qgH52ULy5UNpEn7B7HVT2jpmAttf1k7mARTVWASvZkvajTk2NT5c2x3JHmojB5BDrFV --account.index 0 --subaddr.indices 0 --value 0.5 transfer
+	./mychurnero --dest.address BaqV2s3XmQ9ZP5TkeDqhBJMr3TMe5QV7uQ3uuogrqwKCLqmdMMuojnDgzqzdc34SP68ebH3eURKjM539PArWK9QPRRQK7VZ --account.index 0 --subaddr.indices 0 --value 0.5 transfer
+	./mychurnero --dest.address BeZ1BuKz3WVPpNFpFx3efGUi5sxQLfNbuJgAG2bp1SwPhF5f6P5D4QRFycUx8YJey5P8vxFEYXDRqT4ntYVr8UgLMM5umLo --account.index 0 --subaddr.indices 0 --value 0.5 transfer
+	./mychurnero --dest.address BZ6gABrPHFhboc1GuM7vfbWsT6WJUSHnZNkPBzLdiEXg2TEFE5Zu31T4ejSMjiYthYFAut8sBMqchTXfrba6cvoFPCsZKCU --account.index 0 --subaddr.indices 0 --value 0.5 transfer
+done
